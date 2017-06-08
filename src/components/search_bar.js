1a13ef65d5bd1aa186f8950e2a53356800b51678
Mac Book <home@Macs-MacBook-Pro.local>
2017-06-08 15:44:02 -0700

    user can search for videos

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -10,30 +10,18 @@ class SearchBar extends Component {
     }
 
     render() {
-        // all html input elements emit a change event. This is a normal browser event.
-        // you can just type "on..." and then the name of the event "change" onChange is
-        // a special property in react you could also put the function inline <input
-        // onChange={event => console.log(event.target.value)} />;
         return (
-            <div>
-                {/*you need bind the onInputChange function */}
-                <input
-                    value={this.state.term}
-                    onChange={this
-                    .onInputChange
-                    .bind(this)}/>
+            <div className="search-bar">
+                <input value={this.state.term} onChange={event => this.onInputChange(event.target.value)} />
             </div>
         );
 
     }
 
-    // typically you would name this method whatever the action it is doing all
-    // browser events that get triggered by native inputs, whenever we add an event
-    // handler they are always called with an even OBJECT the event objects
-    // describes the context/ info of the event that occured
-    onInputChange(event) {
-        //update the state by calling setState
-        this.setState({term: event.target.value});
+
+    onInputChange(term) {
+        this.setState({term});
+        this.props.onSearchTermChange(term);
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_details';
+import _ from 'lodash';
 
 import SearchBar from './components/search_bar';
 
@@ -17,9 +18,13 @@ class App extends Component {
       selectedVideo: null
     };
 
-    YTSearch({
+    this.videoSearch('surfboards');
+  }
+
+  videoSearch(term) {
+        YTSearch({
       key: API_KEY,
-      term: 'surfboards'
+      term: term
     }, (videos) => {
       this.setState({
         videos: videos,
@@ -29,9 +34,14 @@ class App extends Component {
   }
 
   render() {
+    //takes the search term and runs every 300ms. 
+    const videoSearch = _.debounce((term) => {
+      this.videoSearch(term)
+    }, 300);
+
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={videoSearch} />
         <VideoDetail video={this.state.selectedVideo}/>
         <VideoList 
         onVideoSelect={selectedVideo => {this.setState({selectedVideo})} }
