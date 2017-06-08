2512c217e4f39c97ec24bba35543941f6643d11a
Mac Book <home@Macs-MacBook-Pro.local>
2017-06-08 12:16:04 -0700

    added video_list_item and used map to loop over videos

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,32 +1,39 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class SearchBar extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
 
-        this.state = { term: '' };
+        this.state = {
+            term: ''
+        };
     }
 
     render() {
-        // all html input elements emit a change event. This is a normal browser event. 
-        // you can just type "on..." and then the name of the event "change"
-        // onChange is a special property in react
-        // you could also put the function inline <input onChange={event => console.log(event.target.value)} />;
+        // all html input elements emit a change event. This is a normal browser event.
+        // you can just type "on..." and then the name of the event "change" onChange is
+        // a special property in react you could also put the function inline <input
+        // onChange={event => console.log(event.target.value)} />;
         return (
-        <div>   
-            {/*you need bind the onInputChange function */}
-            <input value={this.state.term} onChange={this.onInputChange.bind(this)} />
-        </div>
+            <div>
+                {/*you need bind the onInputChange function */}
+                <input
+                    value={this.state.term}
+                    onChange={this
+                    .onInputChange
+                    .bind(this)}/>
+            </div>
         );
-            
+
     }
 
-    //typically you would name this method whatever the action it is doing
-    //all browser events that get triggered by native inputs, whenever we add an event handler they are always called with an even OBJECT
-    //the event objects describes the context/ info of the event that occured
+    // typically you would name this method whatever the action it is doing all
+    // browser events that get triggered by native inputs, whenever we add an event
+    // handler they are always called with an even OBJECT the event objects
+    // describes the context/ info of the event that occured
     onInputChange(event) {
         //update the state by calling setState
-        this.setState({ term: event.target.value });
+        this.setState({term: event.target.value});
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -14,22 +14,40 @@ ReactDOM.render(
   </Provider>
   , document.querySelector('.container'));*/
 
-
-
-
-
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list'
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyCipV7R77EFx-XgSA0N_c4ml_q3fU2BRk8';
 
-const App = function() {
-  return <div>
-    <SearchBar />
-  </div>;
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: []
+    };
+
+    YTSearch({
+      key: API_KEY,
+      term: 'surfboards'
+    }, (videos) => {
+      this.setState({videos});
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
-ReactDOM.render(<App />, document.querySelector('.container'));
+ReactDOM.render(
+  <App/>, document.querySelector('.container'));
