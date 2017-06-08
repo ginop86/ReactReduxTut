28a691f8d425363530d3ecefa9fa3271fd6a7246
Mac Book <home@Macs-MacBook-Pro.local>
2017-06-07 23:28:15 -0700

    Youtube search API key and added setState method

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = { term: '' };
+    }
+
     render() {
         // all html input elements emit a change event. This is a normal browser event. 
         // you can just type "on..." and then the name of the event "change"
         // onChange is a special property in react
         // you could also put the function inline <input onChange={event => console.log(event.target.value)} />;
-        return <input onChange={this.onInputChange}/>;
+        return (
+        <div>   
+            {/*you need bind the onInputChange function */}
+            <input value={this.state.term} onChange={this.onInputChange.bind(this)} />
+        </div>
+        );
+            
     }
 
     //typically you would name this method whatever the action it is doing
     //all browser events that get triggered by native inputs, whenever we add an event handler they are always called with an even OBJECT
     //the event objects describes the context/ info of the event that occured
     onInputChange(event) {
-        console.log(event.target.value);
+        //update the state by calling setState
+        this.setState({ term: event.target.value });
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -20,8 +20,11 @@ ReactDOM.render(
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
-import SearchBar from '../src/components/search_bar'
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyCipV7R77EFx-XgSA0N_c4ml_q3fU2BRk8';
 
 const App = function() {
   return <div>
